a3fa6532dae748ec4fd6708f1606dc72fc8b6dc1
amien kurniawan <offdevamienk@gmail.com>
2020-10-29 11:40:11 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+// function component
+// const Index = () => {
+//   return (
+//     <div>Welcome Page!</div>
+//   )
+// }
+
+// export default Index;
+
+class Index extends Component {
+  render() {
+    return (
+      <div>Welcome Page! from class component</div>
+    )
+  }
+}
+export default Index;

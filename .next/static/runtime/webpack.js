4894b122dd23f5815483e935a2422b241b09ea87
amien kurniawan <offdevamienk@gmail.com>
2020-10-29 11:40:41 +0700

    update commit

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a9ea3274d7826332784f";
+/******/ 	var hotCurrentHash = "c951216ace3f83c70533";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
